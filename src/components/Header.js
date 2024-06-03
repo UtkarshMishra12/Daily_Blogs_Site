2ce45236f860553153d585e537c6eb171a47ff03
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-06-04 02:05:19 +0530

    Blog website
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="py-4 border shadow-md w-full top-0 fixed bg-white">
+            <header className="text-center">
+                <h1 className="font-bold text-3xl uppercase text-center">Daily Blogs!</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
